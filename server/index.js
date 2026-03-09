5e3270fc57be0dac29392bf3634afbed83190335
SupereFr4nz <barridofranz6@gmail.com>
2026-03-09 12:45:49 +0800

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,28 +1,34 @@
-
-
 require("dotenv").config();
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require("express");
 const { connectToMongoDB } = require("./database");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build'))); 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
-});
+app.use(express.static(path.join(__dirname, "build")));
 
 const router = require("./routes");
 app.use("/api", router);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+// ✅ catch-all route
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
-    await connectToMongoDB();
-    app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`);
-    });
+  await connectToMongoDB();
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
 };
+
 startServer();
