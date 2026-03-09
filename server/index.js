853f812819ca29a7bd864463846a86941b1942eb
SupereFr4nz <barridofranz6@gmail.com>
2026-03-09 14:49:16 +0800

    initial commit

--- server/index.js
+++ server/index.js
@@ -8,24 +8,27 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+// serve frontend files
+app.use(express.static(path.join(__dirname, "dist")));
 
 const router = require("./routes");
 app.use("/api", router);
 
+// main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-// ✅ catch-all route
+// ✅ catch-all route (fix)
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectToMongoDB();
+
   app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
   });
