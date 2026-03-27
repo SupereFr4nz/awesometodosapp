abadd0f101a8959fd52593259e1994c933986e96
SupereFr4nz <barridofranz6@gmail.com>
2026-03-27 12:22:49 +0800

    initial commit

--- server/routes.js
+++ server/routes.js
@@ -1,45 +1,78 @@
 const express = require("express");
-
-// create an instance of our router
-const router = express.Router();
-const { getCollection } = require("./models/index");
 const { ObjectId } = require("mongodb");
+const { getCollection } = require("./models/index");
+
+const router = express.Router();
 
 // GET /todos
 router.get("/todos", async (req, res) => {
+  try {
     const collection = getCollection();
     const todos = await collection.find({}).toArray();
-
     res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch todos" });
+  }
 });
+
 
 // POST /todos
 router.post("/todos", async (req, res) => {
+  try {
     const collection = getCollection();
-    let { todo } = req.body;
+    const { todo } = req.body;
 
-    todo = JSON.stringify(todo);
-  
-    const newTodo = await collection.insertOne({ todo, status: false });
-  
-    res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
-  })
+    const newTodo = {
+      todo,
+      status: false
+    };
+
+    const result = await collection.insertOne(newTodo);
+
+    res.status(201).json({
+      _id: result.insertedId,
+      ...newTodo
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create todo" });
+  }
+});
+
+
 // DELETE /todos/:id
 router.delete("/todos/:id", async (req, res) => {
+  try {
     const collection = getCollection();
     const _id = new ObjectId(req.params.id);
-  
-    const deletedTodo = await collection.deleteOne({ _id });
-    res.status(200).json(deletedTodo);
-  })
 
-// PUT /todos/:id
+    const result = await collection.deleteOne({ _id });
+
+    res.status(200).json(result);
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete todo" });
+  }
+});
+
+
+// UPDATE /todos/:id
 router.put("/todos/:id", async (req, res) => {
+  try {
     const collection = getCollection();
     const _id = new ObjectId(req.params.id);
     const { status } = req.body;
-  
-    const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
-    res.status(200).json(updatedTodo);
-  });
+
+    const result = await collection.updateOne(
+      { _id },
+      { $set: { status: !status } }
+    );
+
+    res.status(200).json(result);
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update todo" });
+  }
+});
+
 module.exports = router;
